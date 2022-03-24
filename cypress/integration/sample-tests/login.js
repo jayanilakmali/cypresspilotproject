1d754aa556afe967062cce07505710503e214609
Commerce Promote <commercepromote@Commerces-MacBook-Air.local>
2022-03-24 17:19:09 +0530

    t

--- cypress/integration/sample-tests/login.js
+++ cypress/integration/sample-tests/login.js
@@ -11,59 +11,74 @@ describe('RCP Sign in', () => {
     it.only('Log into RCP', () => {
         cy.visit('/signin')
         cy.get('input[name="username"]')
-            .type("jlakmali+qaautomation@openprovider.nl")
+            .type("jlakmali+mar23@openprovider.nl")
         cy.get('input[name="password"]')
-            .type("Donkey@1")
+            .type("Donkey@321")
         cy.get('button[type="submit"]').click()
-        cy.screenshot()
         cy.wait(5000)
-        cy.contains('Continue ->').click()
         //assert
     })
 
-    it.only('fill personal info', () => {
+    it('fill personal info', () => {
 
         cy.get('input[name="firstName"]').type("QA")
         cy.get('input[name="lastName"]').type("Automation user")
         cy.get('.iti__flag-container').click()
         cy.get('[data-country-code="lk"]').click()
-        cy.wait(5000)
         cy.get('input[type="tel"]').type("775678789")
-        cy.wait(10000)
+        cy.wait(5000)
+        cy.get('button[type="submit"]').click()
+        cy.wait(5000)
+    
+         //cy.screenshot()
+         //assert
+     })
+
+    //  it.only('click submit', () => {
+
+        
+    //     cy.on('uncaught:exception', (err, runnable) => {
+    //         expect(err.message).to.include('Cannot read properties of undefined')
+        
+    //         // using mocha's async done callback to finish
+    //         // this test so we prove that an uncaught exception
+    //         // was thrown
+        
+    //         // return false to prevent the error from
+    //         // failing this test
+    //         return false
+    //       })
+        
+    //       // assume this causes an error
+          
+    //     cy.wait(5000)
+         
+    //   })
+
+ 
+     it.only('select account type', () => {
+        cy.wait(2000)
+         cy.get('input[value="PERSONAL"]').click()
+         cy.wait(5000)
+         cy.get('.button').click('center' , {force : true}, { multiple: true })
+         cy.wait(5000)
+          //assert
+      })
+
+      it.only('Fill more info', () => {
+
+        cy.get('input[name="city"]').type("Colombo")
+        cy.get('input[name="street"]').type("pettah Street")
+        cy.get('input[name="number"]').type("123")
+        cy.get('input[name="zipcode"]').type("11015")
+        cy.get('.background--x-light-grey').click()
+        cy.contains('USD').click()
+        cy.get('button disable[type="submit"]').click()
+
         
     
          //cy.screenshot()
          //assert
      })
 
-     it.only('click submit', () => {
-
-        
-        cy.on('uncaught:exception', (err, runnable) => {
-            expect(err.message).to.include('Cannot read properties of undefined')
-        
-            // using mocha's async done callback to finish
-            // this test so we prove that an uncaught exception
-            // was thrown
-        
-            // return false to prevent the error from
-            // failing this test
-            return false
-          })
-        
-          // assume this causes an error
-          cy.get('button[type="submit"]').click()
-        cy.wait(5000)
-         
-      })
-
- 
-     it.only('select account type', () => {
-        cy.wait(5000)
-         cy.get('input[value="PERSONAL"]').click()
-         cy.get('button[type="submit"]').click()
-         cy.wait(5000)
-         cy.screenshot()
-          //assert
-      })
 })
--- cypress/integration/sample-tests/sign-up.js
+++ cypress/integration/sample-tests/sign-up.js
@@ -1,15 +1,15 @@
 describe('RCP Sign up', () => {
 
-    Cypress.on('uncaught:exception', (err, runnable) => {
-        return false;
-      });
+    // Cypress.on('uncaught:exception', (err, runnable) => {
+    //     return false;
+    //   });
 
-    it('sign up to OP', () => {
+    it.only('sign up to OP', () => {
 
-        //const emailAddress = ('jlakmali+' + (Date.now()/1000) + '@openprovider.nl')
+        const emailAddress = ('jlakmali+' + (Date.now()/1000) + '@openprovider.nl')
         cy.visit('/signup')
         cy.get('input[name="email"]')
-            .type("jlakmali+testsure@openprovider.nl")
+            .type(emailAddress)
         cy.get('input[name="password"]')
             .type("Donkey@1")
         cy.get('.checkmark').click()
@@ -21,13 +21,15 @@ describe('RCP Sign up', () => {
     
     })
 
-    it('fill personal info', () => {
+    it.only('fill personal info', () => {
 
        cy.get('input[name="firstName"]').type("QA")
        cy.get('input[name="lastName"]').type("Automation user")
        cy.get('.iti__flag-container').click()
        cy.get('[data-country-code="lk"]').click()
        cy.get('input[type="tel"]').type("123456780")
+       cy.get('input[name="firstName"]').click()
+       cy.wait(2000)
        cy.get('.button--color-primary').click()
        cy.wait(5000)
        
@@ -36,11 +38,27 @@ describe('RCP Sign up', () => {
         //assert
     })
 
-    it('select account type', () => {
+    it.only('select account type', () => {
         cy.get('input[value="PERSONAL"]').click()
         cy.get('button[type="submit"]').click()
         cy.wait(5000)
         cy.screenshot()
+         //assert
+     })
+
+     it('Fill more info', () => {
+
+        cy.get('input[name="city"]').type("Colombo")
+        cy.get('input[name="street"]').type("pettah Street")
+        cy.get('input[name="number"]').type("123")
+        cy.get('input[name="zipcode"]').type("11015")
+        cy.get('.background--x-light-grey').click()
+        cy.contains('USD').click()
+        cy.get('button[type="submit"]').click()
+
+        
+    
+         //cy.screenshot()
          //assert
      })
 
